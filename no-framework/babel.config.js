42b950cea59068c1bb6cb0d5cea3c9690c39b55d
Joao Almeida <joao-almeida11@outlook.com>
2025-06-04 01:20:02 +0100

    feat: no-framework starter files

---
+++ no-framework/babel.config.js
@@ -0,0 +1,10 @@
+const development = process.env.NODE_ENV || "development" === "development";
+
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-react",
+      { runtime: "automatic", useSpread: true, development },
+    ],
+  ],
+};
