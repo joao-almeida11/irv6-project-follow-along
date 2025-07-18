04873af25b92c16f04e79b20040884e4a421e7d3
Joao Almeida <joao-almeida11@outlook.com>
2025-07-18 05:39:49 +0100

    feat: useDeferredValue

---
+++ deferred/src/Slider.jsx
@@ -0,0 +1,23 @@
+export default function Slider({ value, deferred, onChange, name, max }) {
+  // name !== deferred then its out of sync
+  return (
+    <li className="slider">
+      <label htmlFor={name}>
+        {name}
+        {value !== deferred ? "(Updating)" : ""}
+      </label>
+      <input
+        type="range"
+        id={name}
+        name={name}
+        min="0"
+        max={max}
+        value={value}
+        onChange={onChange}
+      />
+      <output htmlFor="name">
+        Actual Value: {value} | Deferred Value: {deferred}
+      </output>
+    </li>
+  );
+}
